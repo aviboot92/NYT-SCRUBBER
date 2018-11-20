b4f9fcfba899c45d96c23310a17e592c69981404
Venkat-Karra <vkarra@fmnet.ucla.edu>
2018-11-20 13:18:35 -0800

    folder Structure

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a className="navbar-brand" href="/">
+      NEW YORK TIMES ARTICLE SCRUBBER
+    </a>
+  </nav>
+);
+
+export default Nav;
